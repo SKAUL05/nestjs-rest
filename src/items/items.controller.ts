e7eef3e55cb914b581d68fff6b525b5a5851898c
SKAUL05 <kaul.sarath@gmail.com>
2024-03-09 15:31:19 +0530

    First Initialize

---
+++ src/items/items.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+
+@Controller('items')
+export class ItemsController {
+    @Get()
+    findAll(): string {
+        return "Get All Items";
+    }
+    @Post()
+    create(): string {
+        return "Created Items";
+    }
+}

5949d6cf4c8755651e97f2cfc411cdfa7304af03
SKAUL05 <kaul.sarath@gmail.com>
2024-03-09 22:52:56 +0530

    Interface Added

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -9,15 +9,19 @@ import {
   Res,
   Param,
 } from '@nestjs/common';
+import { ItemsService } from './items.service';
+import { Item } from './interfaces/item.interface';
 import { CreateItem } from './dto/create-item.dto';
 import { Request, Response } from 'express';
 
 @Controller('items')
 export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+
   @Get()
   findAll(@Req() req: Request, @Res() res: Response): Response {
     console.log(req.url);
-    return res.send('Get All Items');
+    return res.send(this.itemsService.findAll());
   }
   @Get(':id')
   findOne(@Param('id') id): string {
---
+++ src/items/items.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { Item } from './interfaces/item.interface';
+
+@Injectable()
+export class ItemsService {
+  private readonly items: Item[] = [
+    {
+      id: '343567',
+      name: '1',
+      qty: 100,
+      description: 'Hello',
+    },
+    {
+      id: '343568',
+      name: '2',
+      qty: 100,
+      description: 'Hello',
+    },
+    {
+      id: '343569',
+      name: '3',
+      qty: 100,
+      description: 'Hello',
+    },
+  ];
+  findAll(): Item[] {
+    return this.items;
+  }
+}
